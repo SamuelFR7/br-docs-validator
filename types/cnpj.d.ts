5b07cd7556aac488664c2c7a8ab0c9eabef8a302
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2024-03-15 18:05:49 -0300

    feat: type definitions

---
+++ types/cnpj.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    export { isValid };
+}
+export default _default;
+/**
+ * @param {string} cnpj
+ * @returns {boolean}
+ */
+declare function isValid(cnpj: string): boolean;
+//# sourceMappingURL=cnpj.d.ts.map
---
+++ types/cpf.d.ts
@@ -0,0 +1,10 @@
+declare namespace _default {
+    export { isValid };
+}
+export default _default;
+/**
+ * @param {string} cpf
+ * @returns {boolean}
+ */
+declare function isValid(cpf: string): boolean;
+//# sourceMappingURL=cpf.d.ts.map
